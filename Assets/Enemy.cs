1929178ba2a25ac5b5e89a3c76f2bb4a20615f51
Cameron Robinson <cameron@iMac.local>
2017-07-29 14:29:04 -0700

    Have enemy pathing. Need target selection.

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -2,19 +2,116 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour {
+using Pathfinding;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Seeker))]
+public class Enemy : EnemySpriteController {
+
+	[SerializeField]
+	protected Transform target;
+
+	[SerializeField]
+	protected float updateRate = 2f;
+
+	private Seeker seeker;
+	private Rigidbody2D rBody;
+
+	public Path path;
+	private int currentWaypoint = 0;
+
+	//AI speed
+	public float speed = 300f;
+	public ForceMode2D fMode;
+
+	[HideInInspector]
+	public bool pathIsEnded = false;
+
+	public float nextWaypointDistance = 3;
 
 	// Use this for initialization
 	void Start () {
-		
+
+		this.minMaxY = new Vector2(-4.4f, 0.75f);
+		this.minMaxScale = new Vector2(1.2f, 0.9f);
+
+		this.seeker = this.GetComponent<Seeker>();
+		this.rBody = this.GetComponent<Rigidbody2D>();
+
+		StartCoroutine(this.UpdatePath());
 	}
-	
+
+	void FixedUpdate() {
+
+		if(this.target == null) {
+
+			//TODO: Path around.
+		}
+
+		//TODO: Add look at
+
+		if(path == null)
+			return;
+
+		if(currentWaypoint >= path.vectorPath.Count) {
+			if(pathIsEnded)
+				return;
+				
+			pathIsEnded = true;
+			return;
+		}
+		pathIsEnded = false;
+
+
+		Vector2 dir = (path.vectorPath[currentWaypoint] - this.transform.position).normalized;
+		dir *= this.speed * Time.fixedDeltaTime;
+
+		this.rBody.AddForce(dir, this.fMode);
+
+		float dist = Vector3.Distance(this.transform.position, path.vectorPath[currentWaypoint]);
+		if(dist < nextWaypointDistance) {
+
+			currentWaypoint++;
+		}
+
+
+	}
+
 	// Update is called once per frame
 	void Update () {
-		
+
+
+		float y = Mathf.Clamp(this.transform.position.y, this.minMaxY.x, this.minMaxY.y);
+		this.transform.position = new Vector2(this.transform.position.x, y);
+
+		this.AdjustSpriteScale(this.transform.position);
 	}
 
 	public void TakeDamage() {
 
 	}
+
+	private void OnPathComplete(Path p) {
+
+		if(p.error == false) {
+
+			this.path = p;
+			this.currentWaypoint = 0;
+		}
+	}
+
+	protected IEnumerator UpdatePath() {
+
+		while(true) {
+
+			if(this.target != null) {
+
+				seeker.StartPath(this.transform.position, target.position, OnPathComplete);
+			}
+
+			yield return new WaitForSeconds(1 / updateRate);
+		}
+	}
+
+
 }
--- Assets/EnemySpriteController.cs
+++ Assets/EnemySpriteController.cs
@@ -7,8 +7,7 @@ public class EnemySpriteController : CharacterMovementController {
 
 	void Start() {
 
-		this.minMaxY = new Vector2(-4.4f, 0.75f);
-		this.minMaxScale = new Vector2(1.2f, 0.9f);
+
 	}
 
 	void Update () {
