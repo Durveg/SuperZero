a324bf8c1acabc65170bc92e26c8976a86513f92
Cameron Robinson <cameron@Camerons-iMac.local>
2017-07-28 22:44:19 -0700

    Added working art.
    Added starting scripts to control the sprite transform scale as it moves up and down the screen.

---
+++ Assets/Scripts/Movement Controllers/CharacterMovementController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovementController : MonoBehaviour {
+
+	[SerializeField]
+	protected Transform playerSpriteTransform;
+	[SerializeField]
+	protected Vector2 minMaxY;
+	[SerializeField]
+	protected Vector2 minMaxScale;
+
+	protected float valDist;
+
+	protected void CalcValDist() {
+
+		this.valDist = minMaxScale.y - minMaxScale.x;
+	}
+
+	protected float CalcPercentDistance(float currentDist) {
+
+		float min = this.minMaxY.x;
+		float max = this.minMaxY.y;
+
+		return Mathf.Abs(min - currentDist) / Mathf.Abs(min - max);
+	}
+
+	protected void AdjustSpriteScale(Vector3 characterPos) {
+
+		this.CalcValDist();
+
+		float clampedPos = Mathf.Clamp(characterPos.y, this.minMaxY.x, this.minMaxY.y);
+		float percentDist = this.CalcPercentDistance(clampedPos);
+
+		float scale = minMaxScale.x + (percentDist * this.valDist);
+		scale = Mathf.Clamp(scale, this.minMaxScale.y, this.minMaxScale.x);
+
+		playerSpriteTransform.localScale = new Vector3(scale, scale, 1);
+
+	}
+}
+ 
